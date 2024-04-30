8a54ad6e095414fca24fca4a1583b883773bdcf4
PJ568 <PJ568@PJ568.eu.org>
2024-04-30 11:47:08 +0800

    feat(core): Add preliminary support for PJAX

--- layout/common/head.jsx
+++ layout/common/head.jsx
@@ -170,9 +170,9 @@ module.exports = class extends Component {
             {feeds && feeds.json ? <link rel="alternate" href={url_for(feeds.json)} title={config.title} type="application/json" /> : null}
             {favicon ? <link rel="icon" href={url_for(favicon)} /> : null}
             <link rel="stylesheet" href={iconcdn()} />
-            {hlTheme ? <link rel="stylesheet" href={cdn('highlight.js', '11.7.0', 'styles/' + hlTheme + '.css')} /> : null}
+            {hlTheme ? <link data-pjax rel="stylesheet" href={cdn('highlight.js', '11.7.0', 'styles/' + hlTheme + '.css')} /> : null}
             <link rel="stylesheet" href={fontCssUrl[variant]} />
-            <link rel="stylesheet" href={url_for('/css/' + variant + '.css')} />
+            <link data-pjax rel="stylesheet" href={url_for('/css/' + variant + '.css')} />
             <Plugins site={site} config={config} helper={helper} page={page} head={true} />
 
             {adsenseClientId ? <script data-ad-client={adsenseClientId}
